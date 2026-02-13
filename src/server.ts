8f502953ea1cca79c756b271f5962e366efbe384
mikem131987 <mikem131987@users.noreply.github.com>
2026-02-12 20:12:41 -0600

    Add @types/cors and fix Stripe API version

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16',
+  apiVersion: '2023-08-16',
 });
 
 // Middleware
